6126614313a5814669b44322777e51b5b890d9c3
jacoblmacdonald <jacoblmacdonald@gmail.com>
2015-02-18 17:45:38 -0500

    wolfram

--- client/modules/Wolf.py
+++ client/modules/Wolf.py
@@ -7,31 +7,52 @@ import requests
 import bs4
 from client.app_utils import getTimezone
 from semantic.dates import DateService
-
-from subprocess import call
-call("clear")
-import sys
 import urllib2
 import httplib
 from xml.etree import ElementTree as etree
-
+ 
 WORDS = ["WOLFRAM"]
 
 def handle(text, mic, profile):
-	mic.say("You are in the wolfram module")
-	appid = '4J7JG7-AEVLAH86AP'
-	query = sys.argv[1]
-	print 'Querying: ', query
-	w = wolfram(appid)
-	w.search(query)
+	#mic.say("You are in the wolfram module")
+    appid = '4J7JG7-AEVLAH86AP'
+    query = text
+    w = wolfram(appid)
+    w.search(query)
 
+def isValid(text):
+	return True
+	#if "WOLFRAM" in text:
+	#	return True
+	#return False
 
 class wolfram(object):
-	def __init__(self, appid):
-		self.appid = appid
-		self.base_url = 'http://api.wolframalpha.com/v2/query?'
-		self.headers = {'User-Agent':None}
-def isValid(text):
-	if "WOLFRAM" in text:
-		return True
-	return False
+    def __init__(self, appid):
+        self.appid = appid
+        self.base_url = 'http://api.wolframalpha.com/v2/query?'
+        self.headers = {'User-Agent':None}
+ 
+    def _get_xml(self, ip):
+        url_params = {'input':ip, 'appid':self.appid}
+        data = urllib.urlencode(url_params)
+        req = urllib2.Request(self.base_url, data, self.headers)
+        xml = urllib2.urlopen(req).read()
+        return xml
+ 
+    def _xmlparser(self, xml):
+        data_dics = {}
+        tree = etree.fromstring(xml)
+        #retrieving every tag with label 'plaintext'
+        for e in tree.findall('pod'):
+            for item in [ef for ef in list(e) if ef.tag=='subpod']:
+                for it in [i for i in list(item) if i.tag=='plaintext']:
+                    if it.tag=='plaintext':
+                        data_dics[e.get('title')] = it.text
+        return data_dics
+ 
+    def search(self, ip):
+        xml = self._get_xml(ip)
+        result_dics = self._xmlparser(xml)
+        #return result_dics 
+        #print result_dics
+        print result_dics['Result']
